0ac4a5da6a5245ac1352f4df6f4912bca8e06f7b
audeen <jannik.sievert@gmail.com>
2020-07-14 18:32:37 +0200

    fix errors in server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-/* if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./build"));
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-} */
+}
 app.use(cors());
 app.use(express.json());
 
