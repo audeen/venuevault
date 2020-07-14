cb77e88d7a10d01c6877799f0be68ef04e92be5a
audeen <jannik.sievert@gmail.com>
2020-07-14 18:29:07 +0200

    fix errors in server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use(express.static("./build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"));
   });
-}
+} */
 app.use(cors());
 app.use(express.json());
 
