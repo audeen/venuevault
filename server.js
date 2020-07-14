438a5450d38bad30e5fade6421c8ba7fa490cafc
audeen <jannik.sievert@gmail.com>
2020-07-14 20:07:41 +0200

    buil

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 app.use(cors());
 app.use(express.json());
 
@@ -33,6 +23,17 @@ connection.once("open", () => {
 const venuesRouter = require("./routes/venues");
 
 app.use("/venues", venuesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
