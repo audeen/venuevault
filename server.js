71d8f3e6c64db179fd05ecb5937415704413538c
audeen <jannik.sievert@gmail.com>
2020-07-14 18:26:27 +0200

    fix errors in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
   });
 }
 app.use(cors());
